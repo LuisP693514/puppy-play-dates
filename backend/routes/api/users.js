ee6442740da1498ad6a0e6c751939ca6d83979fc
Luis <perezluis167@gmail.com>
2023-02-21 10:06:46 -0500

    Fixed some errors

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -20,19 +20,19 @@ router.get('/all', async (req, res, next) =>{
   res.json(users);
 })
 
-// /api/users/:userId grabs 1 user specified by the userId in the params
-router.get('/:userId', async (req, res) => {
-  try {
-    const userId = req.params.userId;
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+// // /api/users/:userId grabs 1 user specified by the userId in the params
+// router.get('/:userId', async (req, res) => {
+//   try {
+//     const userId = req.params.userId;
+//     const user = await User.findById(userId);
+//     if (!user) {
+//       return res.status(404).json({ message: 'User not found' });
+//     }
+//     res.json(user);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
 // /api/users/:userId updates the user
 router.patch('/:userId', validateDoggyInputs, async (req, res, next) => {
