2d99f04a7aab9bfc3e5c70b3c225d230794ee645
Janira Crispin <janiracrispin@gmail.com>
2023-02-23 10:58:43 -0500

    test for main copy

--- frontend/src/store/users.js
+++ frontend/src/store/users.js
@@ -2,7 +2,7 @@ import jwtFetch from "./jwt";
 export const RECEIVE_USER = 'RECEIVE_USER';
 export const RECEIVE_USERS = 'RECEIVE_USERS';
 export const REMOVE_USER = 'REMOVE_USER';
-
+//main copy 
 const receiveUser = user => ({
     type: RECEIVE_USER,
     user
