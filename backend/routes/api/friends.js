39189dfe54587c0a30e30be8236d91fe1893da09
Luis <perezluis167@gmail.com>
2023-03-01 12:18:24 -0500

    Added more functionality for friends

--- backend/routes/api/friends.js
+++ backend/routes/api/friends.js
@@ -75,29 +75,46 @@ router.post('/create', async (req, res) => {
 router.delete('/:friendId', async (req, res) => {
     const friendId = req.params.friendId
     try {
-        const friend = await Friend.findByIdAndDelete(friendId)
+        const friend = await Friend.findById(friendId)
         if (!friend) {
             return res.status(404).json({ message: "Friend not found" })
         }
         const user = await User.findById(friend.user)
         const user2 = await User.findById(friend.friend)
+
         if (!user || !user2) {
             return res.status(404).json({message: "User not found"})
         }
         // delete the associations
-        const entry = user.friends.indexOf(friendId)
-        const entry2 = user2.friends.indexOf(friendId)
+        const index1 = user.friends.indexOf(friendId)
+
+        // friend stores the friend as a separate obj
+        // find that id and delete the association + the entry from database
+        const friend2 = await Friend.findOne({$and: [{user: friend.friend},{ friend: friend.user}]})
+
+        const index2 = user2.friends.indexOf(friend2._id)
+
+        if (!friend2) {
+            return res.status(400).json({message: "Friend2 not found"})
+        }
         
-        if (entry > -1){
-            user.friends.splice(entry, 1)
+        if (index1 > -1){
+            user.friends.splice(index1, 1)
         }
 
-        if (entry2 > -1) {
-            user2.friends.splice(entry2, 1)
+        if (index2 > -1) {
+            user2.friends.splice(index2, 1)
         }
+
+        await user.save();
+        await user2.save();
+
+        await friend.remove();
+        await friend2.remove();
 
         res.status(200).json({ message: "Friend deleted" })
     } catch (err) {
+        console.log(err)
         res.status(500).json({ message: err.message })
     }
 })
