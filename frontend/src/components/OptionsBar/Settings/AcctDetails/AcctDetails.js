1b141f642faa5ac4e3dec09c250e13904fcf7014
meihuang <meihuang8328@gmail.com>
2023-03-06 19:31:52 -0500

    added update user account functionality

--- frontend/src/components/OptionsBar/Settings/AcctDetails/AcctDetails.js
+++ frontend/src/components/OptionsBar/Settings/AcctDetails/AcctDetails.js
@@ -1,46 +1,115 @@
 import reactDom from "react-dom"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { getCurrentUser, selectCurrentUser } from "../../../../store/session"
 import { updateUser } from "../../../../store/users"
 import './AcctDetails.css'
 
 export default function AcctDetails({open, onClose}) {
-    const dispatch = useDispatch()
-    const currentUser = useSelector(selectCurrentUser)
+    const dispatch = useDispatch();
+    const currentUser = useSelector(selectCurrentUser);
+      const [editing, setEditing] = useState(null);
 
     useEffect(() => {
         dispatch(getCurrentUser())
     }, [dispatch])
 
     
-  
+    useEffect(() => {
+        setEditing(null);
+    }, [currentUser]);
+
+    const handleEdit = (field) => {
+        setEditing(field);
+    };
+
+    const handleCancel = () => {
+        setEditing(null);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+
+        const updatedUser = {
+            ...currentUser,
+            [editing]: formData.get(editing),
+        };
+
+        await dispatch(updateUser(updatedUser));
+        dispatch(getCurrentUser());
+    };
+
     if (!open) return null
+
     return reactDom.createPortal(
         <>
-            <div>
-                <div className='central-modal'>
-                    <button onClick={onClose} className="modal-close">&times;</button>
-                    <div className="delete-text orange-text"><h3>Update Account Information</h3></div>
-                    <div>
-                        <div className="settings-update">
-                            <div className="settings-div">UserName: {currentUser.username}</div>
-                            <button className="button settings-button">edit</button>
-                        </div>
-                        <div className="settings-update">
-                            <div className="settings-div">Email: {currentUser.email}</div>
-                            <button className="button settings-button">edit</button>
-                        </div>
-                        <div className="settings-update">
-                            <div className="settings-div">Password: **********</div>
-                            <button className="button settings-button">edit</button>
-                        </div>
-                        
-                    </div>
- 
+        <div>
+            <div className='central-modal'>
+            <button onClick={onClose} className="modal-close">&times;</button>
+            <div className="delete-text orange-text"><h3>Update Account Information</h3></div>
+            <form onSubmit={handleSubmit}>
+                <div className="settings-update">
+                <div className="settings-div">
+                    UserName:{" "}
+                    {editing === "username" ? (
+                    <input type="text" name="username" defaultValue={currentUser.username} />
+                    ) : (
+                    currentUser.username
+                    )}
                 </div>
+                {editing === "username" ? (
+                    <>
+                    <button className="button settings-button" type="submit">
+                        Submit
+                    </button>
+                    <button className="button settings-button" onClick={handleCancel}>
+                        Cancel
+                    </button>
+                    </>
+                ) : (
+                    <button className="button settings-button" onClick={() => handleEdit("username")}>
+                    Edit
+                    </button>
+                )}
+                </div>
+                <div className="settings-update">
+                <div className="settings-div">
+                    Email:{" "}
+                    {editing === "email" ? (
+                    <input type="email" name="email" defaultValue={currentUser.email} />
+                    ) : (
+                    currentUser.email
+                    )}
+                </div>
+                {editing === "email" ? (
+                    <>
+                    <button className="button settings-button" type="submit">
+                        Submit
+                    </button>
+                    <button className="button settings-button" onClick={handleCancel}>
+                        Cancel
+                    </button>
+                    </>
+                ) : (
+                    <button className="button settings-button" onClick={() => handleEdit("email")}>
+                    Edit
+                    </button>
+                )}
+                </div>
+                <div className="settings-update">
+                <div className="settings-div">
+                    Password: **********
+                </div>
+                <button className="button settings-button" disabled>
+                    Edit
+                </button>
+                </div>
+            </form>
             </div>
+        </div>
         </>,
         document.getElementById("portal")
-    )
+    );
 }
