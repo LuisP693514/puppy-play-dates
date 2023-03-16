39fb31cc733a85e1a6bd34993852dcde29d1c374
Luis <perezluis167@gmail.com>
2023-03-16 11:43:04 -0400

    Commented out socketio

--- frontend/src/components/TestChat/ChatBox.js
+++ frontend/src/components/TestChat/ChatBox.js
@@ -1,101 +1,101 @@
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { createChatMessage, fetchChatMessages } from '../../store/chatMessages';
-import ScrollToBottom from 'react-scroll-to-bottom';
-import './TestChat.css';
+// import { useEffect, useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { createChatMessage, fetchChatMessages } from '../../store/chatMessages';
+// import ScrollToBottom from 'react-scroll-to-bottom';
+// import './TestChat.css';
 
-const ChatBox = ({ room, user, socket, messages }) => {
+// const ChatBox = ({ room, user, socket, messages }) => {
 
-    const dispatch = useDispatch();
-    const [message, setMessage] = useState({author: user._id, body: '', room: room?._id});
-    const [messageList, setMessageList] = useState(messages)
+//     const dispatch = useDispatch();
+//     const [message, setMessage] = useState({author: user._id, body: '', room: room?._id});
+//     const [messageList, setMessageList] = useState(messages)
 
-    useEffect(() => {
-        socket.on("receive_message", (data) => {
-            setMessageList((list) => [...list, data])
-        })
-        // if (room?._id) {
-        //     dispatch(fetchChatMessages(room._id));
-        // }
-    }, [dispatch])
-    const sendMessage = async () => {
-        // update the message state and create a new message in the backend. do not forget to add 
-        // backend functionality to create a new message and store it in the user's messages array
-        if (message) {
-            const messageData = {
-                author: user._id,
-                body: message.body,
-                room: room?._id,
-                createdAt: new Date(Date.now())
-            }
-            await socket.emit("send_message", messageData)
-            setMessage({author: user._id, body: '', room: room?._id})
-            setMessageList((list) => [...list, messageData])
-            dispatch(createChatMessage(messageData))
-        }
+//     useEffect(() => {
+//         socket.on("receive_message", (data) => {
+//             setMessageList((list) => [...list, data])
+//         })
+//         // if (room?._id) {
+//         //     dispatch(fetchChatMessages(room._id));
+//         // }
+//     }, [dispatch])
+//     const sendMessage = async () => {
+//         // update the message state and create a new message in the backend. do not forget to add 
+//         // backend functionality to create a new message and store it in the user's messages array
+//         if (message) {
+//             const messageData = {
+//                 author: user._id,
+//                 body: message.body,
+//                 room: room?._id,
+//                 createdAt: new Date(Date.now())
+//             }
+//             await socket.emit("send_message", messageData)
+//             setMessage({author: user._id, body: '', room: room?._id})
+//             setMessageList((list) => [...list, messageData])
+//             dispatch(createChatMessage(messageData))
+//         }
 
-    }
+//     }
 
-    return (
-        <div className='chatBox-wrapper'>
-            <div className='chatBox-header'>
-                {/* Should just be the name of the other user that the current user is talking to */}
-                <p>Live Chat</p>
-            </div>
+//     return (
+//         <div className='chatBox-wrapper'>
+//             <div className='chatBox-header'>
+//                 {/* Should just be the name of the other user that the current user is talking to */}
+//                 <p>Live Chat</p>
+//             </div>
 
-            <div className='chat-body'>
-                <ScrollToBottom className='message-container'>
-                    {/* list out all the messages inside the chatroom from oldest to newest */}
-                    {messageList.map(message => (<div className='messageBox' key={message._id}
-                        id={message.author === user._id ? "currentUser" : "otherUser"}>
-                        <p className='message-content'>{message.body}</p>
-                        <div className={'message-time'}>
-                            <p id='date-time-p-tag'>{parseDateTime(message.createdAt)}</p>
-                        </div>
-                    </div>))}
-                </ScrollToBottom>
-            </div>
-            <div className='chatBox-input'>
-                <input
-                    type={'text'}
-                    value={message.body}
-                    onChange={(e) => {
-                        setMessage({
-                            author: user._id,
-                            body: e.target.value,
-                            room: room?._id
-                        })
-                    }}
-                    // Comment this in when the sendMessage function is functional
-                    onKeyDown={(e) => {
-                        if (e.key === 'Enter') {
-                            sendMessage();
-                        }
-                    }}
-                    placeholder={"Aa"}
-                />
-                {/* button that sends the message just in case the user wants to click a box instead of pressing enter */}
-                <button onClick={(e) => {
-                    e.preventDefault();
-                    sendMessage();
-                }}>send</button>
-            </div>
-        </div>
-    )
+//             <div className='chat-body'>
+//                 <ScrollToBottom className='message-container'>
+//                     {/* list out all the messages inside the chatroom from oldest to newest */}
+//                     {messageList.map(message => (<div className='messageBox' key={message._id}
+//                         id={message.author === user._id ? "currentUser" : "otherUser"}>
+//                         <p className='message-content'>{message.body}</p>
+//                         <div className={'message-time'}>
+//                             <p id='date-time-p-tag'>{parseDateTime(message.createdAt)}</p>
+//                         </div>
+//                     </div>))}
+//                 </ScrollToBottom>
+//             </div>
+//             <div className='chatBox-input'>
+//                 <input
+//                     type={'text'}
+//                     value={message.body}
+//                     onChange={(e) => {
+//                         setMessage({
+//                             author: user._id,
+//                             body: e.target.value,
+//                             room: room?._id
+//                         })
+//                     }}
+//                     // Comment this in when the sendMessage function is functional
+//                     onKeyDown={(e) => {
+//                         if (e.key === 'Enter') {
+//                             sendMessage();
+//                         }
+//                     }}
+//                     placeholder={"Aa"}
+//                 />
+//                 {/* button that sends the message just in case the user wants to click a box instead of pressing enter */}
+//                 <button onClick={(e) => {
+//                     e.preventDefault();
+//                     sendMessage();
+//                 }}>send</button>
+//             </div>
+//         </div>
+//     )
 
-}
+// }
 
-const parseDateTime = (dateString) => {
-    const date = new Date(dateString);
+// const parseDateTime = (dateString) => {
+//     const date = new Date(dateString);
 
-    let hours = date.getUTCHours();
-    const minutes = ("0" + date.getUTCMinutes()).slice(-2);
+//     let hours = date.getUTCHours();
+//     const minutes = ("0" + date.getUTCMinutes()).slice(-2);
 
-    const amPM = hours >= 12 ? "PM" : "AM";
-    hours %= 12;
-    hours = hours || 12;
+//     const amPM = hours >= 12 ? "PM" : "AM";
+//     hours %= 12;
+//     hours = hours || 12;
 
-    return (hours + ":" + minutes + " " + amPM + " UTC");
-}
+//     return (hours + ":" + minutes + " " + amPM + " UTC");
+// }
 
-export default ChatBox;
+// export default ChatBox;
--- frontend/src/components/TestChat/index.js
+++ frontend/src/components/TestChat/index.js
@@ -1,112 +1,112 @@
-import './TestChat.css'
-import io from 'socket.io-client'
-import { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { selectCurrentUser } from '../../store/session'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
-import { fetchChatRoom, getChatRoom } from '../../store/chatRooms'
-import { fetchFriends, getFriends } from '../../store/friends'
-import { fetchUser, getUser } from '../../store/users'
-import ChatBox from './ChatBox'
-import { fetchChatMessages, getChatMessages } from '../../store/chatMessages'
+// import './TestChat.css'
+// // import io from 'socket.io-client'
+// import { useEffect, useState } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { selectCurrentUser } from '../../store/session'
+// import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
+// import { fetchChatRoom, getChatRoom } from '../../store/chatRooms'
+// import { fetchFriends, getFriends } from '../../store/friends'
+// import { fetchUser, getUser } from '../../store/users'
+// import ChatBox from './ChatBox'
+// import { fetchChatMessages, getChatMessages } from '../../store/chatMessages'
 
-const socket = io.connect("http://localhost:5001")
+// // const socket = io.connect("http://localhost:5001")
 
-const TestChat = () => {
+// const TestChat = () => {
 
-    // this should work (when completed) when redirected from clicking a specific user
-    // the point is to add the other user to the history state so that the current user has access to the other user's `_id`
-    // this `_id` is used to fectch the rooms that both users have
+//     // this should work (when completed) when redirected from clicking a specific user
+//     // the point is to add the other user to the history state so that the current user has access to the other user's `_id`
+//     // this `_id` is used to fectch the rooms that both users have
 
-    const dispatch = useDispatch();
-    const currentUser = useSelector(selectCurrentUser);
-    const history = useHistory(); // use this to grab 2nd user
-    const user2 = history?.location?.state?.user2
-    const oldRoom = history?.location?.state?.oldRoom
-    const realUser2 = useSelector(getUser(user2))
-    const [updatedUser2, setUpdatedUser2] = useState(false)
-    const [updatedChatRoom, setUpdatedChatRoom] = useState(false)
-    const chatRoom = useSelector(getChatRoom)
-    const messages = useSelector(getChatMessages)
-    const [loadedMessages, setLoadedMessages] = useState(false)
-    const [joinedRoom, setJoinedRoom] = useState(false)
+//     const dispatch = useDispatch();
+//     const currentUser = useSelector(selectCurrentUser);
+//     const history = useHistory(); // use this to grab 2nd user
+//     const user2 = history?.location?.state?.user2
+//     const oldRoom = history?.location?.state?.oldRoom
+//     const realUser2 = useSelector(getUser(user2))
+//     const [updatedUser2, setUpdatedUser2] = useState(false)
+//     const [updatedChatRoom, setUpdatedChatRoom] = useState(false)
+//     const chatRoom = useSelector(getChatRoom)
+//     const messages = useSelector(getChatMessages)
+//     const [loadedMessages, setLoadedMessages] = useState(false)
+//     const [joinedRoom, setJoinedRoom] = useState(false)
 
 
-    //for testing purposes, selecting all friends
+//     //for testing purposes, selecting all friends
 
-    const friends = useSelector(getFriends)
+//     const friends = useSelector(getFriends)
 
-    useEffect(() => {
-        //fetch room passing in 2 users
-        if (user2) {
-            dispatch(fetchUser(user2))
-                .then(() => {
-                    setUpdatedUser2(true)
-                })
-        }
+//     useEffect(() => {
+//         //fetch room passing in 2 users
+//         if (user2) {
+//             dispatch(fetchUser(user2))
+//                 .then(() => {
+//                     setUpdatedUser2(true)
+//                 })
+//         }
 
-        if (realUser2) {
-            grabChatRoom()
-        }
+//         if (realUser2) {
+//             grabChatRoom()
+//         }
 
-        if (chatRoom?._id) {
-            dispatch(fetchChatMessages(chatRoom._id))
-                .then(() => {
-                    setLoadedMessages(true);
-                })
-            joinRoom();
-        }
-        // for testing purposes, fetching all friends
+//         if (chatRoom?._id) {
+//             dispatch(fetchChatMessages(chatRoom._id))
+//                 .then(() => {
+//                     setLoadedMessages(true);
+//                 })
+//             joinRoom();
+//         }
+//         // for testing purposes, fetching all friends
 
-        dispatch(fetchFriends(currentUser?._id));
+//         dispatch(fetchFriends(currentUser?._id));
  
-    }, [dispatch, user2, updatedUser2, updatedChatRoom, joinedRoom])
+//     }, [dispatch, user2, updatedUser2, updatedChatRoom, joinedRoom])
 
-    const grabChatRoom = async () => {
+//     const grabChatRoom = async () => {
 
-        await dispatch(fetchChatRoom(currentUser._id, realUser2._id))
-            .then(() => {
-                setUpdatedChatRoom(true)
-            })
+//         await dispatch(fetchChatRoom(currentUser._id, realUser2._id))
+//             .then(() => {
+//                 setUpdatedChatRoom(true)
+//             })
 
-    }
-    const joinRoom = () => {
-        socket.emit("join_room", { room: chatRoom._id, oldRoom: oldRoom })
-    }
+//     }
+//     const joinRoom = () => {
+//         socket.emit("join_room", { room: chatRoom._id, oldRoom: oldRoom })
+//     }
 
-    if (friends.length === 0) return null;
-    return (
-        <>
-            {/* for testing purposes, displaying all users as a link to chat with them */}
+//     if (friends.length === 0) return null;
+//     return (
+//         <>
+//             {/* for testing purposes, displaying all users as a link to chat with them */}
 
-            <div>
-                <div>friends: </div>
-                {friends.map(friend => {
-                    return (
-                        <>
-                            <button
-                                disabled={user2 === friend.friend}
-                                key={friend._id}
-                                onClick={(e) => {
-                                    setJoinedRoom(false)
-                                    history.push('/test', { user1: currentUser, user2: friend.friend, oldRoom: chatRoom?._id })
-                                }}>{friend.friend}</button>
-                            {(!joinedRoom) && <button
-                                id={"button-to-start-chatting"}
-                                disabled={user2 !== friend.friend}
-                                onClick={(e) => {
-                                    history.push('/test', { user1: currentUser, user2: friend.friend, oldRoom: oldRoom })
-                                    setJoinedRoom(true)
-                                }}>Open Chat</button>}
-                        </>
-                    )
-                })}
-            </div>
+//             <div>
+//                 <div>friends: </div>
+//                 {friends.map(friend => {
+//                     return (
+//                         <>
+//                             <button
+//                                 disabled={user2 === friend.friend}
+//                                 key={friend._id}
+//                                 onClick={(e) => {
+//                                     setJoinedRoom(false)
+//                                     history.push('/test', { user1: currentUser, user2: friend.friend, oldRoom: chatRoom?._id })
+//                                 }}>{friend.friend}</button>
+//                             {(!joinedRoom) && <button
+//                                 id={"button-to-start-chatting"}
+//                                 disabled={user2 !== friend.friend}
+//                                 onClick={(e) => {
+//                                     history.push('/test', { user1: currentUser, user2: friend.friend, oldRoom: oldRoom })
+//                                     setJoinedRoom(true)
+//                                 }}>Open Chat</button>}
+//                         </>
+//                     )
+//                 })}
+//             </div>
 
-            {loadedMessages && joinedRoom ? <ChatBox messages={messages} socket={socket} user={currentUser} room={chatRoom} /> : <></>}
-        </>
-    )
+//             {loadedMessages && joinedRoom ? <ChatBox messages={messages} socket={socket} user={currentUser} room={chatRoom} /> : <></>}
+//         </>
+//     )
 
-}
+// }
 
-export default TestChat;
+// export default TestChat;
